7b4dd373e0266cdff99e191c2eed53319d424f7e
Nadhir Boukorsi <gamemn02@gmail.com>
2018-10-27 21:21:58 +0100

    open WritingActivity when create button is clicked

--- app/src/main/java/dz/deepwork/gamemn02/notekeeper/main/MainActivity.java
+++ app/src/main/java/dz/deepwork/gamemn02/notekeeper/main/MainActivity.java
@@ -1,8 +1,8 @@
 package dz.deepwork.gamemn02.notekeeper.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import dz.deepwork.gamemn02.notekeeper.R;
 import dz.deepwork.gamemn02.notekeeper.data.Note;
 import dz.deepwork.gamemn02.notekeeper.data.NoteRepository;
+import dz.deepwork.gamemn02.notekeeper.writing.WritingActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        NoteRepository noteRepository = NoteRepository.getsInstance();
+        NoteRepository noteRepository = NoteRepository.getInstance();
         ArrayAdapter<Note> noteAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
@@ -36,8 +37,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = WritingActivity.createIntent(
+                        MainActivity.this,
+                        WritingActivity.NOTE_INDEX_NEW_NOTE);
+                startActivity(intent);
             }
         });
     }
