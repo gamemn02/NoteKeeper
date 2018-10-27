81540b85dc6b4b03013dc90e7ec554bb9496f9ec
Nadhir Boukorsi <gamemn02@gmail.com>
2018-10-27 11:52:03 +0100

    create writing activity ui

---
+++ app/src/main/java/dz/deepwork/gamemn02/notekeeper/writing/WritingActivity.java
@@ -0,0 +1,22 @@
+package dz.deepwork.gamemn02.notekeeper.writing;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import dz.deepwork.gamemn02.notekeeper.R;
+
+public class WritingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writing);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
