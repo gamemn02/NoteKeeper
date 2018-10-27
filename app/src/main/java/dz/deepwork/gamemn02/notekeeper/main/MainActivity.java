8a4021aea517b16181895cb18aee97039cc81a57
Nadhir Boukorsi <gamemn02@gmail.com>
2018-10-27 10:34:15 +0100

    package classes by feature

--- app/src/main/java/dz/deepwork/gamemn02/notekeeper/main/MainActivity.java
+++ app/src/main/java/dz/deepwork/gamemn02/notekeeper/main/MainActivity.java
@@ -1,4 +1,4 @@
-package dz.deepwork.gamemn02.notekeeper;
+package dz.deepwork.gamemn02.notekeeper.main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import dz.deepwork.gamemn02.notekeeper.R;
 
 public class MainActivity extends AppCompatActivity {
 
