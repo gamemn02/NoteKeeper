2e0de017c58b3904dc729e2b777526625cebdc72
Nadhir Boukorsi <gamemn02@gmail.com>
2018-10-27 11:19:53 +0100

    fill note ListView from NoteRepository

--- app/src/main/java/dz/deepwork/gamemn02/notekeeper/main/MainActivity.java
+++ app/src/main/java/dz/deepwork/gamemn02/notekeeper/main/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import dz.deepwork.gamemn02.notekeeper.R;
+import dz.deepwork.gamemn02.notekeeper.data.Note;
+import dz.deepwork.gamemn02.notekeeper.data.NoteRepository;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        NoteRepository noteRepository = NoteRepository.getsInstance();
+        ArrayAdapter<Note> noteAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                noteRepository.getNotes());
+        ListView listView = (ListView) findViewById(R.id.list_notes);
+        listView.setAdapter(noteAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
