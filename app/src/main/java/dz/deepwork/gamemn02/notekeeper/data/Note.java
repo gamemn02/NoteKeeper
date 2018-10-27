3e47bb71b64da0f373495807b5c29d612b8fda5a
Nadhir Boukorsi <gamemn02@gmail.com>
2018-10-27 10:37:02 +0100

    add note data template

---
+++ app/src/main/java/dz/deepwork/gamemn02/notekeeper/data/Note.java
@@ -0,0 +1,20 @@
+package dz.deepwork.gamemn02.notekeeper.data;
+
+public class Note {
+
+    private String title;
+    private String body;
+
+    public Note(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
